1b18cd08472b6a70f751ccd57e34ba4ab02a5b2c
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-24 17:20:05 -0500

    adding updates to master

--- src/docs/examples/ProgressBar/example10PercentProgressBar.js
+++ src/docs/examples/ProgressBar/example10PercentProgressBar.js
@@ -3,5 +3,5 @@ import ProgressBar from 'pfreact/ProgressBar';
 
 /** 10% Progress */
 export default function Example10PercentProgressBar() {
-  return <ProgressBar percent={10} width={10} />
+  return <ProgressBar percent={10} width={130} />
 }
--- src/docs/examples/ProgressBar/example90PercentProgressBar.js
+++ src/docs/examples/ProgressBar/example90PercentProgressBar.js
@@ -3,5 +3,5 @@ import ProgressBar from 'pfreact/ProgressBar';
 
 /** 90% Progress */
 export default function Example90PercentProgressBar() {
-  return <ProgressBar percent={90} width={100} />
+  return <ProgressBar percent={90} width={130} />
 }
