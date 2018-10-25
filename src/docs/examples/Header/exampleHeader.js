24b498127f8a15524df516cf3f654a233197f7f6
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-25 17:54:51 -0500

    adding updates to test

--- src/docs/examples/Header/exampleHeader.js
+++ src/docs/examples/Header/exampleHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from 'pfreact/Header';
 
-/** Custom Message */
+/** Header Component */
 export default function ExampleHeader() {
   return <Header header='Pfreact Component Library' />
 }
